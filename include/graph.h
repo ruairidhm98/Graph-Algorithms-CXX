17d80c7e0731a347b0464592c8d71a2fae8eeef9
ruairidhm98 <2250079m@student.gla.ac.uk>
2020-12-27 14:20:18 +0000

    Made code more efficient, using std::move etc

--- include/graph.h
+++ include/graph.h
@@ -31,7 +31,7 @@ protected:
   template <typename Weight>
   inline void addEdge(std::shared_ptr<Vertex>& v1, std::shared_ptr<Vertex>& v2, Weight&& weight)
   {
-    m_edges[v1->getLabel()][v2->getLabel()] = move(Edge(v1, v2, weight));
+    m_edges[v1->getLabel()][v2->getLabel()] = std::move(Edge(v1, v2, weight));
   }
 
   // Reads the file
@@ -43,14 +43,12 @@ protected:
     {
       std::stringstream ss(line);
       int adjacentVertex = 0, edgeWeight;
-      while (ss >> edgeWeight)
+      char b;
+      // Line will be in format e.g. 1(2), vertex 0 has edge to vertex 1 of weight 2
+      while (ss >> adjacentVertex >> b >> edgeWeight >> b)
       {
-        if (edgeWeight != 0)
-        {
-          emplaceEdge(m_vertices[vertex], m_vertices[adjacentVertex], edgeWeight);
-          m_vertices[vertex]->addNeighbour(m_vertices[adjacentVertex]);
-        }
-        ++adjacentVertex;
+        emplaceEdge(m_vertices[vertex], m_vertices[adjacentVertex], std::move(edgeWeight));
+        m_vertices[vertex]->addNeighbour(m_vertices[adjacentVertex]);
       }
       ++vertex;        
     }
@@ -58,6 +56,10 @@ protected:
 
 public:
 
+  // Assume file is in format
+  // line 0| first line is an integer containing the number of vertices
+  // line i| neighbour_v_i_1(edgeWeight) neighbour_v_i_2(edgeWeight) ...
+  // ...
   Graph(const char* filename)
   {
     std::ifstream iFile(filename);
@@ -69,8 +71,12 @@ public:
       m_vertices.emplace_back(i);
       m_edges[i].resize(m_numVertices);
     }
-    
     createEdges(move(iFile));
+  }
+
+  inline Vertex& getVertex(int label)
+  {
+    return *m_vertices[label];
   }
 
   inline Vertex const& getVertex(int label) const
@@ -91,6 +97,27 @@ public:
   inline std::optional<Edge> const& getEdge(int v1, int v2) const
   {
     return m_edges[v1][v2];
+  }
+
+  template <typename Func>
+  void visitVertices(Func&& func)
+  {
+    // Traverse graph and apply function to each vertex
+    for (int i = 0; i < m_numVertices; ++i)
+    {
+      auto&& vertex = getVertex(i);
+      for (auto&& neighbour : vertex.getNeighbours())
+      {
+        if (auto ptrNeighbour = neighbour.lock())
+        {
+          auto result = func(vertex, ptrNeighbour);
+          if (!result)
+          {
+            return;
+          }
+        }
+      }
+    }
   }
 
 };
--- include/network.h
+++ include/network.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "graph.h"
+#include <algorithm>
+#include <numeric>
+#include <vector>
 
-// A network is a directed graph in which edges have capacity. The design explicitly avoids
-// inheritance to avoid (A) complexity and (B) dynamic dispatch/virtual function overhead
+// A network is a directed graph in which edges have capacity and flow. The design intentionally avoids
+// inheritance from Graph to avoid (A) complexity and (B) dynamic dispatch/virtual function overhead
 class Network
 {
 private:
@@ -16,8 +19,41 @@ public:
     : m_graph(filename)
     , m_source(m_graph.getVertex(0))
     , m_sink(m_graph.getVertex(m_graph.getNumVertices()-1))
-    {}
+  {}
 
-  
+  // Returns the true if the graph is a valid flow
+  bool isValidFlow()
+  {
+    const int numVertices = m_graph.getNumVertices();
+    // Sum of incoming edges must be the same as sum of outgoing edges and flow must be > 0
+    std::vector<int> outgoingFlow(numVertices), incomingFlow(numVertices);
+    // Carry out full traversal of graph to compute outgoing and incoming flows for all v in V
+    m_graph.visitVertices([this,&outgoingFlow,&incomingFlow](Vertex &v1, Vertex &v2)
+    {
+      auto const& edge = m_graph.getEdge(v1, v2).value();
+      int const& flow = edge.getFlow();
+      if (flow < 0 || flow > edge.getWeight())
+      {
+        return false;
+      }
+      outgoingFlow[v1.getLabel()] = incomingFlow[v2.getLabel()] = flow;
+      return true;
+    });
+
+    return std::equal(outgoingFlow.begin()+1, outgoingFlow.begin()+numVertices-1, incomingFlow.begin());
+  }
+
+  // Returns the flow in the network. This is the sum of outgoing flows from the source vertex
+  int getFlow() const
+  {
+    auto&& nSource = m_source.getNeighbours();
+    return std::accumulate(nSource.begin(), nSource.end(), 0, [](auto&& v)
+    {
+      if (auto ptr = ve.lock())
+      {
+        sum += m_graph.getEdge(m_source, *ptr).value().getFlow();
+      }
+    });
+  }
 
 };
