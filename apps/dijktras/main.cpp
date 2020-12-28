6625d7783b52c58eb465f6157a642f5c71f9fe1b
ruairidhm98 <2250079m@student.gla.ac.uk>
2020-12-28 14:09:24 +0000

    Started work on dijktras,need to make sepearate branch

--- apps/dijktras/main.cpp
+++ apps/dijktras/main.cpp
@@ -1,12 +1,58 @@
+#include "edge.h"
 #include "graph.h"
-#include <stdio.h>
+#include "vertex.h"
+
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+
+int dijktras(Graph& g, int source, int end)
+{
+  // Use a priority queue to keep track of the vertex with smallest distance
+  std::priority_queue<std::reference_wrapper<Vertex> > minDv;
+  // Keep track of who is visited, this should be a bitset
+  std::vector<bool> visited(g.getNumVertices());
+  // Predecessor array to keep track of actual path
+  std::vector<int> predecessor(g.getNumVertices()), dist(g.getNumVertices());
+
+  // Initialisation
+  for (int i = 0; i < g.getNumVertices(); ++i)
+  {
+    if (i != source)
+    {
+      auto& optEdge = g.getEdge(source, i);
+      if (optEdge.has_value())
+      {
+        auto& edge = optEdge.value();
+        dist[i] = edge.getWeight();
+      }
+      else
+      {
+        dist[i] = INT_MAX;
+      }
+    }
+  }
+
+  predecessor[source] = -1;
+  minDv.push(*g.getVertex(source));
+
+
+}
 
 int main(int argc, char** argv)
 {
-  FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL)
+  if (argc != 4)
   {
-    printf("Fucked it\n");
+    std::cerr << "Please provide 3 arguments, inputFile, source and target vertices";
+    return -1;
   }
+
+  Graph g(argv[1]);
+  int source = std::atoi(argv[2]), target = std::atoi(argv[3]);
+  dijktras(g, source, end);
+
+
   return 0;
 }
